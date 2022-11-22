ec5baa563639132d18a574e1e3c943253763e090
Robson Scoralick <robsonribeirosco2@gmail.com>
2022-11-22 19:10:40 -0300

    att

--- verificador_de_idade/scriptidade.js
+++ verificador_de_idade/scriptidade.js
@@ -10,10 +10,38 @@ function verificar () {
         let sexo = document.getElementsByName('radsex')
         let idade = ano - anoNascimento.value
         let genero = ''
+        let img = document.getElementById('img')
+
         if (sexo[0].checked) {
             genero = 'Masculino'
+            if(idade >= 0 && idade < 12) {
+                //Criança
+                img.src = 'fotos/criança-m.png'
+            } else if (idade < 21) {
+                //Jovem
+                img.src = 'fotos/jovem-m.png'
+            } else if (idade < 50) {
+                //Adulto
+                img.src = 'fotos/adulto-m.png'
+            } else {
+                //Idoso
+                img.src = 'fotos/idoso-m.png'
+            }
         } else if (sexo[1].checked) {
             genero = 'Feminino'
+            if(idade >= 0 && idade < 12) {
+                //Criança
+                img.src = 'fotos/criança-f.png'
+            } else if (idade < 21) {
+                //Jovem
+                img.src = 'fotos/jovem-f.png'
+            } else if (idade < 50) {
+                //Adulto
+                img.src = 'fotos/adulto-f.png'
+            } else {
+                //Idoso
+                img.src = 'fotos/idoso-f.png'
+            }
         }
         resultado.innerHTML = `Detectamos ${genero} com ${idade} anos de idade.`
     }
