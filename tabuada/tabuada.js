74d589a12de83fcb1eeebb86f8786756f5c27558
Robson Scoralick <robsonribeirosco2@gmail.com>
2022-11-28 10:47:45 -0300

    att

--- tabuada/tabuada.js
+++ tabuada/tabuada.js
@@ -1,2 +1,20 @@
-let inputres = document.getElementById('inputres')
-inputres.disabled = true
+function gerarTabuada() {
+    let tab = document.getElementById('seltab')
+    let inputnum = document.getElementById('txtnumber').value
+ 
+    if (inputnum.length == 0) {
+    
+        window.alert('Por favor, digite um número!')
+    
+    } else {
+        tab.innerHTML = "";
+        for (m = 0; m<=10; m++){
+            let item = document.createElement('option')
+            item.text = `${inputnum} x ${m} = ${inputnum*m}`
+            tab.appendChild(item);
+        }
+    }
+    document.getElementById('txtnumber').value= "";
+}
+
+ 
