a347db90778f6230e0316763770fcc9014f79556
Robson Scoralick <robsonribeirosco2@gmail.com>
2022-12-19 13:00:23 -0300

    att

--- analisador_de_numeros/analisador.js
+++ analisador_de_numeros/analisador.js
@@ -1,7 +1,13 @@
-adicionar(){
+let num = []
+
+function adicionar(){
+    num.push(document.getElementById("valor").value)
+    let lista = document.getElementById('lista')
+    
+    console.log(num)
+}
+
+function finalizar(){
 
 }
 
-finalizar(){
-
-}
